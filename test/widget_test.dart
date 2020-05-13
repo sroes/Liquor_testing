417c81ebd4f75498cfaf023e87ea153e0c8816c0
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-13 10:51:58 -0700

    edited widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -22,3 +22,25 @@ void main() {
 
   });
 }
+
+class LiquorMeTimbers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => HomeNotifier(),
+        ),
+        StreamProvider<User>.value(
+          value: AuthService().user,
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+        ),
+        home: Wrapper(),
+      ),
+    );
+  }
