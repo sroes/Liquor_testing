3358d2fe84a5f00a7abe236d907db3c4d088ed3a
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-13 10:43:06 -0700

    edited widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lib/main.dart';
+import 'package:libcd/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
