a0abff19fdb220ced0b24b9e1fcf9f94065df845
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-13 10:36:58 -0700

    edited widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:LiquorMeTimbers/main.dart';
+import 'package:Liquor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
