7d9b0a4f0f62548c23226a75b42a46b2c0192ffc
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-15 13:21:27 -0700

    added widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -38,6 +38,8 @@ void main() {
     expect(find.byWidget(childWidget), findsOneWidget);
   });
 
+////////////
+
 testWidgets('Add and remove a todo', (WidgetTester tester) async {
   // Build the widget
   await tester.pumpWidget(LiquorMeTimbers());
@@ -46,7 +48,32 @@ testWidgets('Add and remove a todo', (WidgetTester tester) async {
   await tester.enterText(find.byType(TextField), 'hi');
 });
   
-  
+testWidgets('Add and remove a todo', (WidgetTester tester) async {
+  // Enter text code...
+
+  // Tap the add button.
+  await tester.tap(find.byType(FloatingActionButton));
+
+  // Rebuild the widget after the state has changed.
+  await tester.pump();
+
+  // Expect to find the item on screen.
+  expect(find.text('hi'), findsOneWidget);
+});
+
+testWidgets('Add and remove a todo', (WidgetTester tester) async {
+  // Enter text and add the item...
+
+  // Swipe the item to dismiss it.
+  await tester.drag(find.byType(Dismissible), Offset(500.0, 0.0));
+
+  // Build the widget until the dismiss animation ends.
+  await tester.pumpAndSettle();
+
+  // Ensure that the item is no longer on screen.
+  expect(find.text('hi'), findsNothing);
+});
+
 }
 
 class LiquorMeTimbers extends StatelessWidget {
