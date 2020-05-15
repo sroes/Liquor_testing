8d445986f12750b5b1d85b038a29f237ea837feb
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-15 14:04:05 -0700

    added tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:liquor/services/auth.dart';
 
 void main() {
 
@@ -48,6 +49,24 @@ testWidgets('Add and remove a todo', (WidgetTester tester) async {
   await tester.enterText(find.byType(TextField), 'hi');
 });
   
+testWidgets('empty email and password doesn\'t call sign in', (WidgetTester tester) async {
+
+    // create a LoginPage
+    AuthService AuthService = new AuthService(title: 'test');
+    // add it to the widget tester
+    await tester.pumpWidget(loginPage);
+
+    // tap on the login button
+    Finder loginButton = find.byKey(new Key('login'));
+    await tester.tap(loginButton);
+
+    // 'pump' the tester again. This causes the widget to rebuild
+    await tester.pump();
+
+    // check that the hint text is empty
+    Finder hintText = find.byKey(new Key('hint'));
+    expect(hintText.toString().contains(''), true);
+  });
 
 }
 
