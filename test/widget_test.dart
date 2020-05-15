123140e7a1ea5cee77cc6b0acb80ebfe4a627987
Scott Roes <scottroes@Scotts-MBP.socal.rr.com>
2020-05-15 11:03:05 -0700

    added widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,22 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows building and interacting
-  // with widgets in the test environment.
+
+  // Defining tests.  The WidgetTester builds and interacts with widgets
   testWidgets('LiquorMeTimbers has a title and message', (WidgetTester tester) async {
     // Create the widget by telling the tester to build it.
     await tester.pumpWidget(LiquorMeTimbers(title: 'T', message: 'M'));
 
-    // Create the Finders.
+    // Creating the Finders.
     final titleFinder = find.text('T');
     final messageFinder = find.text('M');
 
-    // Use the `findsOneWidget` matcher provided by flutter_test to
-    // verify that the Text widgets appear exactly once in the widget tree.
+    // `findsOneWidget` matcher and ' findsOneWidget'
     expect(titleFinder, findsOneWidget);
     expect(messageFinder, findsOneWidget);
   });
+
+  testWidgets('finds a widget using a Key', (WidgetTester tester) async {
+    // Define the test key.
+    final testKey = Key('K');
+
+    // Build a MaterialApp with the testKey.
+    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+    // Find the MaterialApp widget using the testKey.
+    expect(find.byKey(testKey), findsOneWidget);
+  });
+
+  testWidgets('finds a specific instance', (WidgetTester tester) async {
+    final childWidget = Padding(padding: EdgeInsets.zero);
+
+    // Provide the childWidget to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the childWidget in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget);
+  });
+  
 }
 
 class LiquorMeTimbers extends StatelessWidget {
